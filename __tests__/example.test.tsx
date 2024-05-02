e144e1e316f25f46ff3f9187e3d124bd5790fed3
Francisco Cunha <francisccdn@gmail.com>
2024-05-02 12:32:45 +0200

    set up testing with jest

---
+++ __tests__/example.test.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/example";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /home/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
