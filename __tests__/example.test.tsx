e81cfb0f01c28e6fc616ac3659ff1982976cd2ac
Francisco Cunha <francisccdn@gmail.com>
2024-05-02 13:02:48 +0200

    set up e2e testing with cypress

--- __tests__/example.test.tsx
+++ __tests__/example.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Home from "@/pages/example";
+import Home from "@/pages/index";
 
 describe("Home", () => {
   it("renders a heading", () => {
