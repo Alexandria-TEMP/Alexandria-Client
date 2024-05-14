7e2a0747880e51b67e9af26ca1bdd77e047f2605
Francisco Cunha <francisccdn@gmail.com>
2024-05-14 18:20:37 +0200

    fix jest config to consider all files

--- jest.config.ts
+++ jest.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["app/**/*.ts", "app/**/*.tsx"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
