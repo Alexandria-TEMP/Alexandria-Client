070be30b4ea378cac944f81fb1c81d75b7eb0d58
Francisco Cunha <francisccdn@gmail.com>
2024-05-16 16:02:10 +0200

    improve navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AcademicCapIcon } from "@heroicons/react/24/outline";
 import {
   Navbar,
@@ -7,23 +9,53 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import ThemeSwitcher from "./theme-switcher";
+import { usePathname } from "next/navigation";
 
 export default function AlexandriaNavbar() {
+  const pathname = usePathname();
+
+  // Each item in this array becomes an available path in the navbar
+  // href: path or URL to navigate to
+  // label: text rendered in the link
+  const navigationItems = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/newpost",
+      label: "Publish",
+    },
+    {
+      href: "/about",
+      label: "About",
+    },
+  ];
+
   return (
     <Navbar isBordered>
-      {/* Branding */}
-      <NavbarBrand>
-        <AcademicCapIcon className="w-10 h-10 mr-4" />
-        <h1>Alexandria</h1>
-      </NavbarBrand>
-      {/* Main contents */}
-      <NavbarContent>
-        <NavbarItem>
-          <Link href="/">Home</Link>
-        </NavbarItem>
+      {/* Start contents */}
+      <NavbarContent justify="start">
+        {/* Branding */}
+        <NavbarBrand as={Link} href="/">
+          <AcademicCapIcon className="w-10 h-10 mr-4" />
+          <h1>Alexandria</h1>
+        </NavbarBrand>
+      </NavbarContent>
+      {/* Center contents */}
+      <NavbarContent justify="center">
+        {
+          // Render each navigation item with its label, setting them as
+          // active when their link is open
+          navigationItems.map(({ href, label }) => (
+            <NavbarItem key={href} isActive={pathname === href}>
+              <Link href={href}>{label}</Link>
+            </NavbarItem>
+          ))
+        }
       </NavbarContent>
       {/* End contents */}
-      <NavbarContent>
+      <NavbarContent justify="end">
         <NavbarItem>
           <ThemeSwitcher />
         </NavbarItem>
