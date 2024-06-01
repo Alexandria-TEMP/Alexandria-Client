93bad70f8f26f44f1db80fedc28cc5f3b4299995
Francisco Cunha <francisccdn@gmail.com>
2024-06-01 16:39:54 +0200

    add render pending

--- app/post/[id]/components/post-body/version-render/component.tsx
+++ app/post/[id]/components/post-body/version-render/component.tsx
@@ -8,6 +8,7 @@ import Error from "./error";
 import { setupResize, changeColors } from "./lib/iframe-manipulator";
 import { useTheme } from "next-themes";
 import { semanticColors } from "@nextui-org/react";
+import RenderPending from "./render-pending";
 
 /**
  * Isolated iframe with a Version's rendered html.
@@ -22,6 +23,7 @@ export default function VersionRender({ id }: IdProp) {
   // Component status
   const [isLoaded, setLoaded] = useState(false);
   const [failed, setFailed] = useState(false);
+  const [pending, setPending] = useState(false);
 
   // Used to trigger a rerender in case of an error
   const [rerender, setRerender] = useState(false);
@@ -36,6 +38,7 @@ export default function VersionRender({ id }: IdProp) {
 
   const reset = () => {
     setFailed(false);
+    setPending(false);
     setLoaded(false);
     setIframeHeight(300);
     setRerender(!rerender);
@@ -45,7 +48,11 @@ export default function VersionRender({ id }: IdProp) {
   useEffect(() => {
     getRenderedVersion(id)
       .then((res) => {
-        setHtml(res);
+        if (res === "pending") {
+          setPending(true);
+        } else {
+          setHtml(res);
+        }
       })
       .catch((reason) => {
         console.log(
@@ -90,6 +97,10 @@ export default function VersionRender({ id }: IdProp) {
 
   if (failed) {
     return <Error reset={reset} />;
+  }
+
+  if (pending) {
+    return <RenderPending refresh={reset} />;
   }
 
   return (
