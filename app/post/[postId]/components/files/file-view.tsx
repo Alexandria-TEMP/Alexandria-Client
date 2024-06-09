280e533cf20f8dbabd7c6cde302fbe6fc85e0264
Francisco Cunha <francisccdn@gmail.com>
2024-06-09 21:56:29 +0200

    make file view a client component

--- app/post/[postId]/components/files/file-view.tsx
+++ app/post/[postId]/components/files/file-view.tsx
@@ -1,15 +1,36 @@
-import { getFileContents } from "@/lib/api-calls/version-api";
+"use client";
+
+import DefaultError from "@/error";
+import { useFileContents } from "@/lib/api-hooks/version-hooks";
 import { parseId } from "@/lib/string-utils";
 import { IdProp } from "@/lib/types/react-props/id-prop";
+import GenericLoadingPage from "@/loading";
+import { useState } from "react";
 
-export default async function FileView({
+/**
+ * Displays contents of some file in a version
+ * @param id version ID
+ * @param path filepath
+ */
+export default function FileView({
   id,
   path,
 }: IdProp &
   Readonly<{
     path: string;
   }>) {
-  const data = await getFileContents(parseId(id), path);
+  const { data, error, isLoading } = useFileContents(parseId(id), path);
+  // Used to trigger a rerender in case of an error
+  const [rerender, setRerender] = useState(false);
+
+  if (error) {
+    return <DefaultError error={error} reset={() => setRerender(!rerender)} />;
+  }
+
+  if (isLoading) {
+    return <GenericLoadingPage />;
+  }
+
   return (
     <div className="px-2 border-2 bg-default-100 rounded-md font-mono">
       {data}
