75dff70df30810c2d94fc2e789511ea346f1c62e
Francisco Cunha <francisccdn@gmail.com>
2024-06-16 12:55:06 +0200

    update sidebar parameters

--- app/post/[postId]/(branch)/version/[branchId]/components/branch-parts/branch-sidebar.tsx
+++ app/post/[postId]/(branch)/version/[branchId]/components/branch-parts/branch-sidebar.tsx
@@ -2,7 +2,7 @@ import ChipList from "@/components/common/chip-list";
 import Sidebar from "@/components/layout/sidebar";
 import { fetchBranchData } from "@/lib/api/services/branch-api";
 import { idT } from "@/lib/types/api-types";
-import { IdProp } from "@/lib/types/react-props/id-prop";
+import { idBranchUnionT } from "@/lib/types/branch-union";
 import AuthorCardList from "@/post/[postId]/components/cards/author-card-list";
 import PostCardMini from "@/post/[postId]/components/cards/post-card-mini";
 
@@ -10,9 +10,13 @@ import PostCardMini from "@/post/[postId]/components/cards/post-card-mini";
  * Sidebar for a branch.
  * Displays its post, scientific field list and contributor list.
  * @param id branch ID
+ * @param isClosed indicates if branch is a closed branch
  */
-export default async function BranchSidebar({ id }: IdProp) {
-  const data = await fetchBranchData(id as idT);
+export default async function BranchSidebar({
+  id,
+  isClosed,
+}: Readonly<idBranchUnionT>) {
+  const data = await fetchBranchData({ id: id as idT, isClosed });
 
   return (
     <Sidebar
