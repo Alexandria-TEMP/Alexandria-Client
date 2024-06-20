c95288d30d34a9761c010ecaf8e880f16bb5deb2
Francisco Cunha <francisccdn@gmail.com>
2024-06-20 14:48:13 +0200

    fix missing render bug

--- app/lib/api/hooks/quarto-hooks.ts
+++ app/lib/api/hooks/quarto-hooks.ts
@@ -6,7 +6,7 @@ import { FileTreeT } from "../../types/file-tree";
 import { QuartoContainerT } from "../../types/quarto-container";
 import { validateResponse } from "../api-common";
 import { useMemo } from "react";
-import { buildFetchConfig, buildResourcePath } from "../services/quarto-api";
+import { buildResourcePath } from "../services/quarto-api";
 
 /**
  * Fetches HTML render of a Quarto project.
@@ -24,10 +24,18 @@ export function useRender(
   const swrResponse: SWRResponse<string, Error> = useSWR(
     `${buildResourcePath(container)}/render`,
     async (...args) => {
-      const res = await fetch(...args, buildFetchConfig(container.type));
+      const res = await fetch(...args, {
+        cache: "no-store",
+        headers: [["Accept", "text/html"]],
+      });
       if (res.status === 202) return "pending";
       await validateResponse(res);
-      return res.text();
+
+      console.log(res);
+      const text = await res.text();
+      console.log(text);
+
+      return text;
     },
   );
 
@@ -55,7 +63,7 @@ export function useFileTree(
   container: QuartoContainerT,
 ): SWRResponse<FileTreeT, Error> {
   return useSWR(`${buildResourcePath(container)}/tree`, async (...args) => {
-    const res = await fetch(...args, buildFetchConfig(container.type));
+    const res = await fetch(...args, { cache: "no-store" });
     await validateResponse(res);
     const responseTree = (await res.json()) as { [key: string]: number };
     return parseFileTree(responseTree);
@@ -81,7 +89,7 @@ export function useFileContents(
     async (...args) => {
       if (path === "") return "";
 
-      const res = await fetch(...args, buildFetchConfig(container.type));
+      const res = await fetch(...args, { cache: "no-store" });
       await validateResponse(res);
       return res.text();
     },
