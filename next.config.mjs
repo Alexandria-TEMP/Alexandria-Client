c38cc9727f7eaff574142f5a7fe745c3179000e8
Julia van der Kris <juliavdkris@proton.me>
2024-07-31 17:38:32 +0200

    lol nevermind that breaks stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-
     // DOCKER: If you're developing using a Docker container, uncomment the following 
     // lines to enable hot-reloading. Hot-reloading also works with this configueration
     // when using Node locally, but changes show up faster without it.
